2394e86b0872d8b4b389d79e31f5c6df9a34cd36
abodell <andybodell9@gmail.com>
2026-02-24 20:03:42 -0500

    Small fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     """
 
     ENABLE_SCHEDULER: bool = False
-    ENABLE_GOLFNOW: bool = True
     SCAN_INTERVAL_SECONDS: int = 60
     ENVIRONMENT: str = "development"
     GOOGLE_MAPS_API_KEY: str = ""
